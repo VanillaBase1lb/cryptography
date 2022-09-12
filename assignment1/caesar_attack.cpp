abcf901522da014cfe55fdf8a1ff6fdc08e7ee23
Yuvraj Mishra <40194399+VanillaBase1lb@users.noreply.github.com>
2022-09-12 15:39:57 +0530

    add timer for cracking

--- assignment1/caesar_attack.cpp
+++ assignment1/caesar_attack.cpp
@@ -1,10 +1,21 @@
 #include "attack_common.cpp"
+#include <chrono>
 
 int main(int argc, char *argv[]) {
   std::string cipher_text = readCipherFromFile();
-  std::cout << "Key: " << crackCaesar(cipher_text).first << std::endl;
-  std::cout << "Deciphered text: " << crackCaesar(cipher_text).second
-            << std::endl;
+  // start timer
+  auto start = std::chrono::high_resolution_clock::now();
+  int key = crackCaesar(cipher_text).first;
+  std::string deciphered_text = crackCaesar(cipher_text).second;
+  // end timer
+  auto finish = std::chrono::high_resolution_clock::now();
+  // print time
+  std::chrono::duration<double> elapsed = finish - start;
+  std::cout << "Elapsed time: " << elapsed.count() << std::endl;
+
+  std::cout << "Key: " << key << std::endl;
+  std::cout << "Deciphered text: " << deciphered_text << std::endl;
 
   return 0;
 }
+
--- assignment1/vigenere_attack.cpp
+++ assignment1/vigenere_attack.cpp
@@ -1,10 +1,21 @@
 #include "attack_common.cpp"
+#include <chrono>
 
 int main(int argc, char *argv[]) {
   std::string cipher_text = readCipherFromFile();
-  std::cout << "Key: " << crackVigenere(cipher_text).first << std::endl;
-  std::cout << "Deciphered text: " << crackVigenere(cipher_text).second
-            << std::endl;
+  // start timer
+  auto start = std::chrono::high_resolution_clock::now();
+  std::string key = crackVigenere(cipher_text).first;
+  std::string deciphered_text = crackVigenere(cipher_text).second;
+  // end timer
+  auto finish = std::chrono::high_resolution_clock::now();
+  // print time
+  std::chrono::duration<double> elapsed = finish - start;
+  std::cout << "Elapsed time: " << elapsed.count() << std::endl;
+
+  std::cout << "Key: " << key << std::endl;
+  std::cout << "Deciphered text: " << deciphered_text << std::endl;
 
   return 0;
 }
+
